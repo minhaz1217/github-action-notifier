5d4fafffa972ac8d53431fe0196b052f0a745ead
Minhaz <minhaz1217@gmail.com>
2024-01-14 17:30:13 +0600

    theme working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
+"use client";
+import { Button } from "primereact/button";
 import Image from "next/image";
 import GithubApiKeyInput from "../component/GithubApiKeyInput";
 
 export default function Home() {
   return (
-    <h1>
-      Github Action Notifier
-      <GithubApiKeyInput />
-    </h1>
+    <Button
+      className="form-button cyan-400"
+      label="Button"
+      style={{ marginTop: "2rem" }}
+    />
+    // <h1>
+    //   Github Action Notifier
+    //   {/* <GithubApiKeyInput /> */}
+    // </h1>
   );
 }
