91c8d7b0d1c1b6e6356b35ec44503dee2ca87040
Minhaz <minhaz1217@gmail.com>
2024-01-14 17:37:03 +0600

    Github key input working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,5 @@
-"use client";
-import { Button } from "primereact/button";
-import Image from "next/image";
 import GithubApiKeyInput from "../component/GithubApiKeyInput";
 
 export default function Home() {
-  return (
-    <Button
-      className="form-button cyan-400"
-      label="Button"
-      style={{ marginTop: "2rem" }}
-    />
-    // <h1>
-    //   Github Action Notifier
-    //   {/* <GithubApiKeyInput /> */}
-    // </h1>
-  );
+  return <GithubApiKeyInput />;
 }
--- src/component/GithubApiKeyInput.tsx
+++ src/component/GithubApiKeyInput.tsx
@@ -1,5 +1,30 @@
+"use client";
+import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
+import { useState } from "react";
+
 const GithubApiKeyInput = () => {
-  return <h1>Inside github api key</h1>;
+  const [apiKey, setApiKey] = useState<string>("");
+
+  const onSaveClicked = (e: any) => {
+    console.debug("Key", apiKey);
+  };
+  return (
+    <>
+      <InputText
+        placeholder="Github Token"
+        value={apiKey}
+        onChange={(e) => setApiKey(e.target.value)}
+      />
+      <Button
+        className="ml-2"
+        label="Save"
+        type="button"
+        icon="pi pi-check"
+        onClick={onSaveClicked}
+      />
+    </>
+  );
 };
 
 export default GithubApiKeyInput;
