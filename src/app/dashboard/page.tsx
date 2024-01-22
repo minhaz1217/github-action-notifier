82b800c33170d163676b3f5e659d324402a85867
Minhaz <minhaz1217@gmail.com>
2024-01-23 02:42:14 +0600

    main page moved to dashboard

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+import DataObserver from "../../backend/patterns/DataObserver";
+import GithubApiKeyInput from "../../component/GithubApiKeyInput";
+import RepoList from "../../component/RepoList";
+import SubscriptionList from "../../component/SubscriptionList";
+import { register } from "../../instrumentation";
+
+const Dashboard = () => {
+  const newSubscriptionAdded$ = new DataObserver();
+  register();
+  return (
+    <>
+      <div className="p-4">
+        <GithubApiKeyInput />
+        <div className="mt-2"></div>
+        <div className="flex flex-row">
+          <div className="basis-1/4 border-e-2 border-e-black pe-2">
+            <SubscriptionList
+              updateOnSubscriptionChanged={newSubscriptionAdded$}
+            />
+          </div>
+          <div className="basis-3/4 px-2">
+            <RepoList notifySubscriptionChanged={newSubscriptionAdded$} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,7 @@
-"use client";
-import DataObserver from "../backend/patterns/DataObserver";
-import GithubApiKeyInput from "../component/GithubApiKeyInput";
-import RepoList from "../component/RepoList";
-import SubscriptionList from "../component/SubscriptionList";
-import { register } from "../instrumentation";
-
 export default function Home() {
-  const newSubscriptionAdded$ = new DataObserver();
-  register();
   return (
     <>
-      <div className="p-4">
-        <GithubApiKeyInput />
-        <div className="mt-2"></div>
-        <div className="flex flex-row">
-          <div className="basis-1/4 border-e-2 border-e-black pe-2">
-            <SubscriptionList
-              updateOnSubscriptionChanged={newSubscriptionAdded$}
-            />
-          </div>
-          <div className="basis-3/4 px-2">
-            <RepoList notifySubscriptionChanged={newSubscriptionAdded$} />
-          </div>
-        </div>
-      </div>
+      <a href="/login">Please Go To Login Page</a>
     </>
   );
 }
