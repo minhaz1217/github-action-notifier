bf1ec718f154b7c1c6df7c24fca714c4ac71ba52
Minhaz <minhaz1217@gmail.com>
2024-01-26 02:05:50 +0600

    blocked non logged in on dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,7 @@
 "use client";
+import { useRouter } from "next/navigation";
 import DataObserver from "../../backend/patterns/DataObserver";
+import UserService from "../../backend/services/UserService";
 import GithubApiKeyInput from "../../component/GithubApiKeyInput";
 import RepoList from "../../component/RepoList";
 import SubscriptionList from "../../component/SubscriptionList";
@@ -8,25 +10,32 @@ import { register } from "../../instrumentation";
 
 const Dashboard = () => {
   const newSubscriptionAdded$ = new DataObserver();
+  const userService = new UserService();
+  const { push } = useRouter();
+  if (!userService.isLoggedIn()) {
+    push("/login");
+  }
   register();
   return (
-    <>
-      <ToolBar />
-      <div className="p-4">
-        {/* <GithubApiKeyInput /> */}
-        <div className="mt-2"></div>
-        <div className="flex flex-row">
-          <div className="basis-1/4 border-e-2 border-e-black pe-2">
-            <SubscriptionList
-              updateOnSubscriptionChanged={newSubscriptionAdded$}
-            />
-          </div>
-          <div className="basis-3/4 px-2">
-            <RepoList notifySubscriptionChanged={newSubscriptionAdded$} />
+    userService.isLoggedIn() && (
+      <>
+        <ToolBar />
+        <div className="p-4">
+          {/* <GithubApiKeyInput /> */}
+          <div className="mt-2"></div>
+          <div className="flex flex-row">
+            <div className="basis-1/4 border-e-2 border-e-black pe-2">
+              <SubscriptionList
+                updateOnSubscriptionChanged={newSubscriptionAdded$}
+              />
+            </div>
+            <div className="basis-3/4 px-2">
+              <RepoList notifySubscriptionChanged={newSubscriptionAdded$} />
+            </div>
           </div>
         </div>
-      </div>
-    </>
+      </>
+    )
   );
 };
 
