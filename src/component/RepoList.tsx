3de2b1047c07d146f94b148e642cb1e085e34a39
Minhaz <minhaz1217@gmail.com>
2024-01-28 02:30:57 +0600

    loader added

--- src/component/RepoList.tsx
+++ src/component/RepoList.tsx
@@ -4,6 +4,7 @@ import GithubTokenService from "../backend/services/GithubTokenService";
 import { Button } from "primereact/button";
 import { Octokit } from "@octokit/rest";
 import { InputText } from "primereact/inputtext";
+import { ProgressSpinner } from "primereact/progressspinner";
 import RepoBasic from "./RepoBasic";
 import Repository from "../backend/repository/Repository";
 import Tables from "../backend/Tables";
@@ -11,13 +12,13 @@ import { RepoModel } from "../backend/models/RepoModel";
 import { Subscription } from "../backend/domain/Subscription";
 import pb from "../backend/db";
 import { INotifier } from "../backend/patterns/DataObserver";
-import Discord from "../backend/notification-providers/Discord";
 
 const RepoList = ({
   notifySubscriptionChanged,
 }: {
   notifySubscriptionChanged: INotifier;
 }) => {
+  const [loading, setLoading] = useState<boolean>(false);
   const [apiKey, setApiKey] = useState<string | null>(null);
   const [repoSearchText, setRepoSearchText] = useState<string>("");
   const [searchResult, setSearchResult] = useState<any[] | null>(null);
@@ -27,32 +28,31 @@ const RepoList = ({
   }, [apiKey]);
 
   const checkApiKey = async () => {
+    setLoading(true);
     const token = await githubApiKeyService.get();
     if (token && apiKey !== token.value) {
       setApiKey(token.value);
     }
     console.debug("API KEY", token);
+    setLoading(false);
   };
   const onSearchClicked = async (e: FormEvent) => {
     e.preventDefault();
     if (apiKey === null || repoSearchText.trim() === "") {
       return;
     }
-
+    setLoading(true);
     const octokit = new Octokit({
       auth: apiKey,
     });
     const searchResult = await octokit.search.repos({
       q: `${repoSearchText}`,
     });
+    setLoading(false);
     setSearchResult(searchResult.data.items);
     console.debug("Result", searchResult.data.items);
-    // const orgs = await octokit.request("GET /user/repos", commonHeader);
-    // console.debug("ORGS", orgs);
-    // const commits = await octokit.repos.listCommits();
   };
   const onSubscribeClicked = async (repoDetails: RepoModel) => {
-    console.debug("Subscribe Clicked", repoDetails);
     const subscriptionRepository = new Repository(Tables.SUBSCRIPTION);
     const payload: Subscription = {
       githubId: repoDetails.id,
@@ -72,6 +72,8 @@ const RepoList = ({
       await subscriptionRepository.create<Subscription>(payload);
       notifySubscriptionChanged.notifyAll();
     }
+
+    setLoading(false);
   };
   return (
     <div>
@@ -87,11 +89,14 @@ const RepoList = ({
           type="button"
           icon="pi pi-search"
           onClick={onSearchClicked}
+          disabled={loading}
           rounded
         />
       </form>
+      {loading && <ProgressSpinner />}
       <div>
-        {searchResult &&
+        {!loading &&
+          searchResult &&
           searchResult.map((item) => (
             <RepoBasic
               isSubscribed={false}
@@ -100,6 +105,7 @@ const RepoList = ({
               buttonClicked={onSubscribeClicked}
             />
           ))}
+        {searchResult && searchResult.length === 0 && <span>Not Found</span>}
       </div>
     </div>
   );
