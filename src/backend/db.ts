7d63300ae2397df5e1b10340a6891e8c5f182f3f
Minhazul Hayat Khan <minhaz1217@gmail.com>
2024-12-28 03:43:30 +0600

    added prod and dev urls

--- src/backend/db.ts
+++ src/backend/db.ts
@@ -1,3 +1,3 @@
 import PocketBase, { BaseAuthStore } from "pocketbase";
-const pb = new PocketBase("http://127.0.0.1:8090").autoCancellation(false);
+const pb = new PocketBase(process.env.NEXT_PUBLIC_POCKET_BASE_URL).autoCancellation(false);
 export default pb;
